2262eb12c98fc8889e18fb845a15a3a8aaa1ad2f
Marco <marco.pacurariu@rocketmail.com>
2017-05-09 21:23:25 +0300

    //laborator 9 mai_2

---
+++ WebApplication2/Controllers/BuildingController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class BuildingController : Controller
+    {
+        ApplicationDbContext ob = new ApplicationDbContext();
+
+        // GET: Building
+        public ActionResult Index()
+        {
+            var userID = this.User.Identity.GetUserId();
+            var user = ob.Users.Find(userID);
+            var city = user.Cities.First();
+            return View(city);
+            //return View();
+        }
+        public ActionResult Build(int buildingID)
+        {
+            return View(new BuildViewModel
+            {
+                BuildingID = buildingID,
+                BuildingTypes = this.ob
+                    .BuildingTypes
+                    .Select(b => new SelectListItem
+                    {
+                        Value = b.BuildingTypeId.ToString(),
+                        Text = b.Name
+                    })
+            });
+        }
+    }
+
+    public class BuildViewModel
+    {
+        public int BuildingID { get; set; }
+        public IEnumerable <SelectListItem> BuildingTypes { get; set; }
+        public int? SelectedBuildingType { get; set; }
+    }
+}
---
+++ WebApplication2/Migrations/Configuration.cs
@@ -0,0 +1,55 @@
+namespace WebApplication2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using WebApplication2.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication2.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "WebApplication2.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(WebApplication2.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            context.BuildingTypes.AddOrUpdate(
+                p => p.Name,
+                new BuildingType { Name = "Granar" },
+                new BuildingType { Name = "Hambar" },
+                new BuildingType { Name = "Cazarma" }
+                );
+
+            var cities = context.Cities.ToList();
+
+            foreach (var city in cities)
+            {
+                for(int i=0; i<10; i++)
+                {
+                    var building = city.Buildings.ElementAtOrDefault(i);
+                    if(building==null)
+                    {
+                        building = new Building { City = city };
+                        city.Buildings.Add(building);
+                    }
+                }
+            }
+        }
+    }
+}
